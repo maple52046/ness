12096c90b2a8cbdd677e24e5279339483d290d7c
Ku Chen-Hao <maple52046@gmail.com>
2018-01-09 11:57:15 +0800

    Add feature and optimize code
    1. Using async module to merge data (data from mariadb and influxdb)
    2. Optimize code
    3. Update feature (the '/strategy' route)
    4. Rename original Python module (from "pair" to "ratio")
    5. Add new Python module with original code (new "pair" module)

--- external_modules/analyzers/algorithm/pair.py
+++ external_modules/analyzers/algorithm/pair.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import json
+import dateutil
 
-def is_pair(a, b):
+def ratio(a, b):
 	assert isinstance(a, float)
 	assert isinstance(b, float)
 
@@ -10,33 +11,48 @@ def is_pair(a, b):
 		(a, b) = (b, a)
 
 	ratio = float(a)/float(b)
-	return ratio if ratio >= 0.9 else None
+	return ratio 
 
-def main(stocks):
-	assert isinstance(stocks, dict)
-	stocks = list(stocks.items())
-	stocks.sort()
+def trading_pair(primary, secondary, data):
+	assert isinstance(primary, str)
+	assert isinstance(secondary, str)
+	assert isinstance(data, list)
+	stocks = {primary: [], secondary: []}
+	pairs = []
 
-	pairs = {}
-	for i in range(len(stocks)-1):
-		for j in range((i+1), len(stocks)):
-			ch1, p1 = stocks[i]
-			ch2, p2 = stocks[j]
-			ratio_value = is_pair(float(p1), float(p2))
-			if ratio_value:
-				pairs.setdefault(ch1, {})
-				pairs.setdefault(ch2, {})
-				pairs[ch1][ch2] = ratio_value
-				pairs[ch2][ch1] = ratio_value
+	for stock in data:
+		# Filter data
+		try:
+			channel = stock.pop('channel')
+			stocks[channel].append(stock)
+		except KeyError:
+			# Ignore incorrect data
+			pass
 
+		if len(stocks[primary]) >= 1 and len(stocks[secondary]) >= 1:
+			# Create pair
+			p = stocks[primary].pop(0)
+			s = stocks[secondary].pop(0)
+
+			date = [p['time'], s['time']]
+			date.sort()
+			pair = {
+				"value": float(p["price"])/float(s["price"]),
+				"time": date[1]
+			}
+			pairs.append(pair)
+	
 	return pairs
 	
 
 if __name__ == "__main__":
 	import sys
 
-	stocks = json.loads(sys.argv[1])
-	print(json.dumps(main(stocks)))
+	stocks = []
+	with open(sys.argv[3], 'r') as f:
+		stocks = json.loads(f.read())
+
+	print(json.dumps(trading_pair(sys.argv[1], sys.argv[2], stocks)))
 	
 
 
--- routes/twse.js
+++ routes/twse.js
@@ -1,137 +1,172 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+var router = express.Router()
+const fs = require('fs')
+const uuid = require('uuid/v4')
+const async = require('async');
 
-var mariadbClient = require('mariasql');
+// MariaDB client
+const mariadbClient = require('mariasql')
 const mariadb = new mariadbClient({
 	host: "localhost",
 	user: "ness",
 	password: "ness",
 	db: "ness",
 	charset :"utf8"
-});
+})
 
-const Influx = require("influx");
+// InfluxDB client
+const Influx = require("influx")
 const influx = new Influx.InfluxDB({
 	host: 'localhost',
 	database: 'ness'
-});
+})
 
-var pysh = require('python-shell');
+// Python shell
+const pysh = require('python-shell')
+
+// Epothtime function
+var epochTime = function (hour, minute){
+	var newDate = new Date()
+	newDate.setHours(hour, minute, 0, 0)
+	return newDate.getTime()*1000*1000
+}
+
+var dateToEpochTime = function (dateString) {
+	var patten = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/
+	var date = new Date(dateString.replace(patten, "$1-$2-$3T$4:$5:$6Z"))
+	return date.getTime()*1000*1000
+}
+
+var dateRangeToEpochTime = function (startDateString, endDateString){
+	/* If the string of start date is null, using today's moring (9:00) as the start date*/
+	if (startDateString != null){
+		var start = dateToEpochTime(startDateString)
+	}else{
+		var start = marketOpen = epochTime(9, 0)
+	}
+
+	/* If the string of end date is null, using today's afternoon (13:30) as the end date*/
+	if (endDateString != null){
+		var end = dateToEpochTime(endDateString)
+	}else{
+		var end = epochTime(13, 30)
+	}
+
+	return [start, end]
+}
+
+var tmpFile = function (data){
+	var filePath = '/tmp/ness-' + uuid()
+	fs.writeFile(filePath, data, (err) => {
+		if (err){
+			console.log(err);
+		}
+	})
+	return filePath
+}
 
 const grafanaUrl = {
 	"prefix": "/grafana/dashboard-solo/db/summary?orgId=1&from=",
 	"suffix": "&theme=light&panelId=1"
 }
 
-var epochTime = function (hour, minute){
-	var newDate = new Date();
-	newDate.setHours(hour, minute, 0, 0);
-	return newDate.getTime();
-};
 
-router.get('/stocks', function (req, res, next) {
-	// Set response is a JSON data
-	res.setHeader('Content-Type', 'application/json');
-
-	// Query stock list from MariaDB
-	const sql = "select symbol,name from stock_list where tag = 'tw0050'";
+var StockName = function (tag, callback){
+	const sql = "select symbol,name from stock_list where tag = '" + tag + "'"
+	var stocks = new Object()
 	mariadb.query(sql, (err, results) => {
 		if (err){
-			console.log(err);
-			// Return an empty json object when query is fault.	
-			res.json({});
-			return;
+			console.log(err)
+			callback(err, null)
+		}else{
+			results.forEach(result => {
+				stocks[result.symbol] = result.name
+			})
+			callback(null, stocks)
 		}
-		res.json(results);
-	});
-});
+	})
+	mariadb.end()
+}
+
+router.get('/stocks', function (req, res, next) {
+	res.setHeader('Content-Type', 'application/json')
+
+	/* Get stock name and channel from MariaDB (with tag name) */
+	const tag = req.query.tag || 'tw0050'
+	const [start, end] = dateRangeToEpochTime(req.query.start, req.query.end)
+
+	async.series([
+		function(next){
+			StockName(tag, function(err, stocks){
+				next(null, stocks)
+			})
+		},
+		function(next){
+			const qs = 'show tag values from twse with key in ("channel") where time >= ' + start + " and time <= " + end
+			influx.query(qs).then(results => {
+				next(null, results)
+			})
+		}
+	], function(errs, results){
+		var [stocks, channels] = results
+		var stockList = new Array()
+		channels.forEach(channel => {
+			stockList.push({"label": stocks[channel["value"]], "value": channel["value"]})
+		})
+		res.json(stockList)
+	})
+
+})
 
 router.get('/intraday', function(req, res, next) {
 	// Get Stock from HTTP request
-	var symbol = req.query.symbol;
+	var symbol = req.query.symbol
 
 	// Set the time range
-	var marketOpen = epochTime(9, 0);
-	var marketClosed = epochTime(13, 30);
+	var marketOpen = epochTime(9, 0)
+	var marketClosed = epochTime(13, 30)
 
 	// Set the grafana url
 	var url = grafanaUrl["prefix"] + marketOpen + "&to=" + marketClosed + 
-		"&var-channel=" + symbol + grafanaUrl["suffix"];
-	res.render('intraday', { "grafana_url": url });
-});
+		"&var-channel=" + symbol + grafanaUrl["suffix"]
+	res.render('intraday', { "grafana_url": url })
+})
 
-var dateToEpochTime = function (dateString) {
+router.get('/strategy', function(req, res, next){
+	res.setHeader('Content-Type', 'application/json')
+	var primary = req.query.primary
+	var secondary = req.query.secondary
+	var [start, end] = dateRangeToEpochTime(req.query.start, req.query.end)
+	//var stockObj = {}
 
-	var patten = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
-	var date = new Date(dateString.replace(patten, "$1-$2-$3T$4:$5:$6Z"));
-	return date.getTime()*1000*1000;
-	
-}
+	// Generate InfluxDB query command
+	var queryString = "select channel,price from twse where time >= " + start + " and time <= " + end + " and channel =~ /" + primary + "|" + secondary + "/"
+	console.log(queryString)
+	influx.query(queryString).then(stockPrices => {
 
-var dateRangeToEpochTime = function (startDateString, endDateString){
-	return [dateToEpochTime(startDateString), dateToEpochTime(endDateString)];
-}
+		//res.json(stockPrices)
 
-router.get('/backtest', function(req, res, next){
-	// time range
-	var start = dateToEpochTime(req.query.start);
-	var end = dateToEpochTime(req.query.end);
-
-	// symbol
-	var symbol = req.query.symbol.split('-');
-
-	// get data from database
-	var queryString = "select channel, price from twse where time >= " + start + " and time <= " + end + 
-		" and (channel = '" + symbol[0] + "' or channel = '" + symbol[1] + "')";
-	influx.query(queryString).then(stocks => {
+		// Prepare Python runtime environment
+		var dataFile = tmpFile(JSON.stringify(stockPrices))
 		var options = {
 			mode: 'json',
 			pythonPath: '/usr/bin/python3',
 			pythonOptions: '-u',
-			scriptPath: 'external_modules/analyzers/algorithm',
-			args: [JSON.stringify(stocks)]
-		};
+			scriptPath: 'external_modules/',
+			args: [primary, secondary, dataFile]
+		}
 
-		pysh.run('stretage.py', options, function(err, results) {
-			if (err) res.json({});
-			res.json(results);
-		});
-	});
-});
-
-router.get('/backtest/pair', function(req, res, next){
-	var [start, end] = dateRangeToEpochTime(req.query.start, req.query.end);
-	var queryString = "select channel,price from twse where time >= " + start + " and time <= " + end;
-	var stockObj = {};
-	influx.query(queryString).then(stocks => {
-		stocks.forEach(function(stock){
-			var price = parseFloat(stock['price']);
-			var channel = stock['channel'];
-			if (stockObj.hasOwnProperty(channel) == true){
-				if (stockObj[channel] < price){
-					stockObj[channel] = price;
-				}
-			}else{
-				stockObj[channel] = price;
+		// Run Python program to analysis stock
+		pysh.run('analyzers/algorithm/pair.py', options, function(err, results) {
+			if (err) {
+				console.log(err)
+				res.json({})
 			}
-		});
+			res.json(results[0])
+		})
+	})
+})
 
-		var options = {
-			mode: 'json',
-			pythonPath: '/usr/bin/python3',
-			pythonOptions: '-u',
-			scriptPath: 'external_modules/analyzers/algorithm',
-			args: [JSON.stringify(stockObj)]
-		};
-
-		pysh.run('pair.py', options, function(err, results) {
-			if (err) res.json({});
-			//if (err) throw err;
-			res.json(results);
-		});
-	});
-});
-
-module.exports = router;
+module.exports = router
 
 // vim: ts=4 sw=4
